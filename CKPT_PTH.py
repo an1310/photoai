d5772d3ae8da5e0ee2833e38528c08c9d50db554
Erik Salter <c.erik.salter@gmail.com>
2025-06-26 16:38:33 +0000

    backup

---
+++ CKPT_PTH.py
@@ -0,0 +1,4 @@
+LLAVA_CLIP_PATH = '/opt/aiplus/models/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = '/opt/aiplus/models//AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
+SDXL_CLIP1_PATH = '/opt/aiplus/models//AIGC_pretrain/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = '/opt/aiplus/models//AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
